6979e8317067bd3105afca1fb4643118a2654491
arjunsinghyadav2 <arjun.yadav@anheuser-busch.com>
2021-01-27 20:26:47 -0800

    adding final itteration with lat long arg

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ This project is a continuation of the Backyard Flyer project where you executed 
 1. Writeup / README that includes all the rubric points and how I addressed each one.
 
 the Starter Code
-1. motion_planning.py 
-  The code contains a motion planner class that contains helper function that help in arming the drone, takeoff and transitioning to waypoints, the core of the code is a A* algorithem that helps in planning path from point A to point B selected by user. Here also the local, home and global position of the drone are also constantly received in the code. 
+  motion_planning.py 
+  The code contains a motion planner class that contains helper function that help in arming the drone, takeoff and transitioning to waypoints, the core of the code is a A* algorithm that helps in planning path from point A to point B selected by user. Here also the local, home and global position of the drone are also constantly received in the code. 
   
   planning_utils.py:
-  the code contains tools which creating a grid using the collider.csv data as well as Action that our drone can possibly take along with cost of each action, these action are using in A* algo. Lastly the code contains a heurestic menthod that alows us to calculate distance between two points and adding the distance to our cost for path planning.
+  the code contains tools which creating a grid using the collider.csv data as well as Action that our drone can possibly take along with cost of each action, these action are using in A* algorithm . Lastly the code contains a heuristic method that allows us to calculate distance between two points and adding the distance to our cost for path planning.
 
 
 And here's a lovely image of my results 
@@ -20,33 +20,28 @@ And here's a lovely image of my results
 
 ![alt text](https://github.com/arjunsinghyadav2/flyingcar/blob/master/image1.jpg?raw=true)
 
-Here's	A	Snappy	Table
-1	highlight	bold	7.41
-2	a	b	c
-3	italic	text	403
-4	2	3	abcd
-Implementing Your Path Planning Algorithm
-1. Set your global home position
-Here students should read the first line of the csv file, extract lat0 and lon0 as floating point values and use the self.set_home_position() method to set global home. Explain briefly how you accomplished this in your code.
 
-And here is a lovely picture of our downtown San Francisco environment from above! Map of SF
+1. Set your global home position
+Line 129 (motion_planning.py)
 
 2. Set your current local position
-Here as long as you successfully determine your local position relative to global home you'll be all set. Explain briefly how you accomplished this in your code.
-
-Meanwhile, here's a picture of me flying through the trees! Forest Flying
+line 132 (motion_planning.py)
 
 3. Set grid start position from local position
-This is another step in adding flexibility to the start location. As long as it works you're good to go!
+Line 148 (motion_planning.py)
 
 4. Set grid goal position from geodetic coords
 This step is to add flexibility to the desired goal location. Should be able to choose any (lat, lon) within the map and have it rendered to a goal location on the grid.
+Line 150 (motion_planning.py)
 
 5. Modify A* to include diagonal motion (or replace A* altogether)
-Minimal requirement here is to modify the code in planning_utils() to update the A* implementation to include diagonal motions on the grid that have a cost of sqrt(2), but more creative solutions are welcome. Explain the code you used to accomplish this step.
+Minimal requirement here is to modify the code in planning_utils() to update the A* implementation to include diagonal motions on the grid that have a cost of sqrt(2), but more creative solutions are welcome. Explain the code you used to accomplish this step. 
+class Action(Enum): Line 58-61 (planning_utils.py)
+
 
 6. Cull waypoints
 For this step you can use a collinearity test or ray tracing method like Bresenham. The idea is simply to prune your path of unnecessary waypoints. Explain the code you used to accomplish this step.
+Line 161 (motion_planning.py)
 
 Execute the flight
 1. Does it work?
--- motion_planning.py
+++ motion_planning.py
@@ -24,13 +24,14 @@ class States(Enum):
 
 class MotionPlanning(Drone):
 
-    def __init__(self, connection):
+    def __init__(self, connection, goal_position=None):
         super().__init__(connection)
 
         self.target_position = np.array([0.0, 0.0, 0.0])
         self.waypoints = []
         self.in_mission = True
         self.check_state = {}
+        self.goal_position = goal_position
 
         # initial state
         self.flight_state = States.MANUAL
@@ -126,8 +127,10 @@ class MotionPlanning(Drone):
         lon = float(lat_lon.columns[1][6:])
         # TODO: set home position to (lon0, lat0, 0)
         self.set_home_position = (lon, lat, 0)
-        # TODO: retrieve current global position
         print("home position set to {0}, {1}".format(lon,lat))
+        # TODO: retrieve current global position
+        local_north, local_east, local_down = global_to_local(self.global_position, self.global_home)
+        print("local position set to {0}, {1}, {2}".format(local_north,local_east,local_down))       
         # TODO: convert to current local position using global_to_local()
         local_coordinates_NED = global_to_local( self.global_position, self.global_home)
         print('local_coordinates_NED {0}'.format(local_coordinates_NED))
@@ -136,32 +139,28 @@ class MotionPlanning(Drone):
                                                                          self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64',skiprows=2)
-        
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        #grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        gtl = global_to_local(self.global_position,self.global_home)
-        grid_start = (-north_offset+int(gtl[0]),-east_offset+int(gtl[1]))
+        grid_start = (int(np.ceil(local_north - north_offset)), int(np.ceil(local_east - east_offset)))
         # Set goal as some arbitrary position on the grid
-        gl = [-122.392836,37.794050]
-        desired = global_to_local([gl[0],gl[1],10.0],self.global_home)
-        grid_goal = ( -north_offset+int(gtl[0]) + int(desired[0]), -east_offset+int(gtl[1])+ int(desired[1]))
+        goal = global_to_local(self.goal_position,self.global_home)
+        grid_goal = ( int(np.ceil(goal[0]-north_offset)), int(np.ceil(goal[1]-east_offset)))
         # TODO: adapt to set goal as latitude / longitude position and convert
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
-        #path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+        path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
-        #print('path: ',path)
-        #pruned_path = prune_path(path)
-        #print('pruned path: ',pruned_path)
+        print('path: ',path)
+        pruned_path = prune_path(path)
+        print('pruned path: ',pruned_path)
         # TODO (if you're feeling ambitious): Try a different approach altogether!
-        pruned_path = [(364, 507), (364, 465), (385, 444), (415, 444), (425, 434), (445, 434), (465, 454), (495, 424), (525, 424), (535, 414), (585, 414), (595, 424), (605, 414), (645, 414), (655, 404), (675, 404), (685, 394), (705, 394), (716, 405), (723, 398), (724, 398), (741, 381), (750, 381), (752, 379), (766, 379), (770, 383), (771, 383), (775, 387), (776, 387), (788, 375), (801, 375), (804, 378), (810, 372), (820, 372), (824, 368), (837, 368), (838, 369), (838, 425), (839, 426), (839, 515), (862, 538), (862, 555), (872, 565), (872, 601), (874, 603), (874, 616), (872, 618), (880, 626), (880, 639), (875, 644), (875, 649), (872, 652), (872, 660), (861, 671), (860, 671), (835, 696), (817, 696), (805, 708), (799, 708), (785, 722), (777, 722), (773, 726), (766, 726), (764, 728), (761, 728), (749, 740), (742, 740), (740, 742), (733, 742), (724, 751), (716, 751), (710, 757), (705, 757), (702, 760), (700, 760), (694, 766), (691, 766), (580, 877), (576, 877), (564, 889), (563, 889), (540, 912)]
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in pruned_path]
         # Set self.waypoints
@@ -186,10 +185,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5760, help='Port number')
     parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    parser.add_argument('--goal_lon', type=str, help="goal lon")
+    parser.add_argument('--goal_lat', type=str, help="goal lat")
+    parser.add_argument('--goal_alt', type=str, help="goal alt")
     args = parser.parse_args()
 
     conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
-    drone = MotionPlanning(conn)
+    goal_position = np.fromstring(f'{args.goal_lon},{args.goal_lat},{args.goal_alt}', dtype='Float64', sep=',')
+    drone = MotionPlanning(conn, goal_position=goal_position)
     time.sleep(1)
 
     drone.start()
